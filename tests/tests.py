5e3c1b800031da041d1cea0564ef57097025adb1
guerrad49 <63264459+guerrad49@users.noreply.github.com>
2024-04-14 07:54:56 -0400

    minor

--- tests/tests.py
+++ tests/tests.py
@@ -1,18 +1,18 @@
 import sys
 # caution: path[0] is reserved for script path (or '' in REPL)
-sys.path.insert(1, '/Users/david_guerra/Documents/Programming/python/gyms')
+sys.path.insert(1, '/Users/david_guerra/Documents/Programming/python/PokemonGo.py')
 
 import unittest
 
 import pandas as pd
-from gyms import Gym
+from PokemonGo import GoogleSheet, Image, Gym
 
 BADGES = '/Users/david_guerra/Documents/Programming/python/gyms/badges'
 
-class TestBirthday(unittest.TestCase):
+class Tests(unittest.TestCase):
     def setUp(self):
         self.df = pd.read_csv('test.csv', index_col=0)
-        self.img01 = Gym('IMG_1187.PNG', self.df)
+        self.img01 = Gym('IMG_0001.PNG', self.df)
 
     def some_test(self):
         pass
