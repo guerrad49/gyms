5e885e3ad2e4e3b7e05fdfcde0e6845d04a1346a
guerrad49 <63264459+guerrad49@users.noreply.github.com>
2024-04-15 18:44:36 -0400

    initial set up script

---
+++ setup.sh
@@ -0,0 +1,33 @@
+echo "SETUP SCRIPT"
+echo "------------\n"
+
+echo "Creating a storage directory at path below..."
+echo "\t$(pwd)/badges\n"
+mkdir badges
+sleep 1   # wait
+
+echo "Creating a virtual environment at path below..."
+echo "\t$(pwd)/.venv\n"
+python3 -m venv .venv
+sleep 1   # wait
+
+echo "Activating virtual environment..."
+source .venv/bin/activate
+echo ""
+sleep 1   # wait
+
+echo "Upgrading pip..."
+python3 -m pip install --upgrade pip
+echo ""
+sleep 1   # wait
+
+echo "Installing the following libraries:"
+cat subfiles/requirements.txt
+echo ""
+python3 -m pip install -r subfiles/requirements.txt
+echo ""
+sleep 1   # wait
+
+deactivate
+
+echo "Set up complete."
