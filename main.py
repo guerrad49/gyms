cf037638cc0756f98c90f50ac3ef325e943bc3a7
guerrad49 <63264459+guerrad49@users.noreply.github.com>
2024-04-23 18:49:48 -0400

    moved functions to utils

--- main.py
+++ main.py
@@ -4,66 +4,11 @@ import os
 import sys
 import argparse
 
-from dotenv import load_dotenv    # load env vars
-
-from PokemonGo import GoogleSheet, Image, Gym, ColorPrint
-
-
-#=============================GLOBAL VARIABLES================================
-
-DOWNLOADS = os.path.join(os.getenv('HOME'), 'Downloads')
-BADGES    = os.path.join(os.path.dirname(__file__), 'badges')
+from PokemonGo import utils
+from PokemonGo import GoogleSheet, Image, Gym
 
 
 #===============================FUNCTIONS=====================================
-
-def has_valid_environment():
-    subfiles = os.path.join(os.path.dirname(__file__), 'subfiles')
-
-    # load environment
-    env_path = os.path.join(subfiles, 'variables.env')
-    load_dotenv(env_path)
-    
-    global KEYFILE, SHEET, AGENT
-    
-    # check json key file exits
-    KEYFILE = os.path.join(subfiles, os.getenv("KEYFILE"))
-    if not os.path.isfile(KEYFILE):
-        prompt = "error: could not locate '{}' file".format(KEYFILE)
-        ColorPrint(prompt).fail()
-        return False
-    
-    # check remainding env variables
-    prompt = "error: 'variables.env' not properly set"
-
-    if (SHEET := os.getenv("SHEET")) == "":
-        ColorPrint(prompt).fail()
-        return False
-    
-    if (AGENT := os.getenv("EMAIL")) == "":
-        ColorPrint(prompt).fail()
-        return False
-
-    return True
-
-def get_queue() -> list:
-    """Returns True when successfully populated a queue to scan"""
-
-    queue = [
-        os.path.join(DOWNLOADS, x) for x in os.listdir(DOWNLOADS) \
-        if x.endswith('.PNG')
-        ]
-
-    if len(queue) == 0:
-        print('INFO - No images found.\n')
-    else:
-        prompt  = 'INFO - Found the following images:\n'
-        prompt += '\n'.join(queue)
-        prompt += '\n'
-        print(prompt)
-    
-    return queue
-    
     
 def parse_args():
     command_desc = 'Scan badge data from PNG files.'
@@ -85,22 +30,20 @@ def parse_args():
 if __name__ == '__main__':
     args = parse_args()
 
-    if not has_valid_environment():
-        sys.exit()
+    utils.load_env()
     
-    queue = get_queue()
+    queue = utils.get_queue()
     if len(queue) == 0:
-        ColorPrint('---Processor ended---\n').fail()
-        sys.exit()
+        sys.exit('---Processor ended---\n')
 
-    gs = GoogleSheet(KEYFILE, SHEET)
+    gs = GoogleSheet(os.environ['KEY_PATH'], os.environ['SHEET_NAME'])
     gs.establish_connection()
     gs.records_to_dataframes()
 
     next_id = gs.processed['image'].max() + 1
     ids = range(next_id, next_id + len(queue))
 
-    ColorPrint('\nINFO - Begin scanning process.\n').proc()
+    print('\nINFO - Begin scanning process.\n')
 
     for id, path in zip(ids, queue):
         img = Image(path)
@@ -113,10 +56,10 @@ if __name__ == '__main__':
 
         g = Gym(id)
         g.set_fields_from_image(img_data)
-        g.set_location_fields(coords, AGENT)
+        g.set_location_fields(coords, os.environ['EMAIL'])
 
         gym_row = g.format_fields()
-        img.to_storage(BADGES, id)
+        img.to_storage(os.environ['BADGES'], id)
 
         gs.write_row(ridx, gym_row)
         print()
