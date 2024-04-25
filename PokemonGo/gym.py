e4ee2956878e0cf3d33a45c95d5959e81c5410a7
guerrad49 <63264459+guerrad49@users.noreply.github.com>
2024-04-24 20:27:51 -0400

    major rework

--- PokemonGo/gym.py
+++ PokemonGo/gym.py
@@ -1,68 +1,81 @@
+"""
+PokemonGo.gym
+"""
+
+from typing import Optional
+
 from geopy.geocoders import Nominatim
 
 
+HRS_IN_DAY = 24
+MINS_IN_DAY = 1400
 LONG_TERM_DEFENDING = 100   # in days
 
 
-class Gym:
-    '''A container to manage all gym-related fields'''
+class GoldGym:
+    """
+    An instance of this class represents a container for 
+    managing gym-related information.
+    """
     
-    def __init__(self, image_id: int):
-        '''
+    def __init__(
+        self, 
+        uid:       int,
+        title:     Optional[str]       = 0,
+        victories: Optional[str | int] = 0,
+        days:      Optional[str | int] = 0,
+        hours:     Optional[str | int] = 0,
+        minutes:   Optional[str | int] = 0,
+        treats:    Optional[str | int] = 0
+    ) -> None:
+        self.uid       = uid
+        self.title     = title
+        self.style     = None
+        self.victories = int(victories)
+        self.days      = int(days)
+        self.hours     = int(hours)
+        self.minutes   = int(minutes)
+        self.defended  = 0
+        self.treats    = int(treats)
+        
+        """
         Parameters
         ----------
-        image_id:
-            The value identifying a gym with an image
-        '''
-        self.image = image_id
-    
-
-    def set_fields_from_image(self, data: dict):
-        '''
-        Helper method to set multiple gym fields.
-
-        Parameters
-        ----------
-        data:
-            The gym values extracted from image text
-        '''
-
-        self.set_title(data)
-        self.set_victories(data)
-        self.set_time_defended(data)
-        self.set_treats(data)
-        self.set_style()
+        uid:
+            The unique id number relating an image to a gym
+        title:
+            The gym title
+        victories:
+            The number of victories at a gym
+        days:
+            The number of days defending a gym
+        hours:
+            The number of additional hours defending a gym
+        minutes:
+            The number of additional minutes defending a gym
+        treats:
+            The number of treats fed at a gym
+        """
 
 
-    def set_title(self, d: dict):
-        self.title = d['title']
+    def set_defended(self) -> None:
+        """
+        Compute total time defended from time attributes. 
+        User should ensure time attributes have been set. 
+        Otherwise, time defended will be 0.
+        """
+
+        total = self.days + self.hours / HRS_IN_DAY \
+            + self.minutes / MINS_IN_DAY
+
+        self.defended = round(total, 4)   # in days
 
 
-    def set_victories(self, d: dict):
-        self.victories = int(d['victories'])
-
-
-    def set_time_defended(self, d: dict):
-        '''Compute total time defended from time components'''
-
-        # create formatted subset of dictionary
-        subd = {k:d[k] for k in ['days','hours','minutes']}
-        subd = {k:0 if v is None else int(v) for k,v in subd.items()}
-        
-        self.days    = subd['days']
-        self.hours   = subd['hours']
-        self.minutes = subd['minutes']
-        
-        total = self.days + self.hours / 24 + self.minutes / 1440
-        self.defended = round(total, 4)
-
-
-    def set_treats(self, d: dict):
-        self.treats = int(d['treats'])
-
-
-    def set_style(self):
-        '''Determine style depending on time defended'''
+    def set_style(self) -> None:
+        """
+        Determine gym style based on number of days defended. 
+        Default is `gold` style.
+        """
 
         if self.days >= LONG_TERM_DEFENDING:
             self.style = '100+ days'
@@ -70,34 +83,34 @@ class Gym:
             self.style = 'gold'
 
 
-    def set_location_fields(self, coordinates: str, email: str):
-        '''Helper method to set all gym location fields'''
-        
-        self.set_address(coordinates, email)
-        self.set_city()
-        self.set_county()
-        self.set_state()
-
-
-    def set_address(self, coordinates: str, email: str):
-        '''
-        Set address dictionary using third party library.
+    def set_address(self, coordinates: str, email: str) -> None:
+        """
+        Set address dictionary.
 
         Parameters
         ----------
         coordinates:
-            The known coordinates with `lat,long` format
+            The known coordinates in `lat,long` format
         email:
             The user's email required by third party ToS
-        '''
+        """
 
         self.coordinates  = coordinates
         geolocator        = Nominatim(user_agent=email)
-        location          = geolocator.reverse(self.coordinates.split(','))
-        self.address      = location.raw['address']   # dictionary
+
+        # proactive format clean up
+        coords_list  = [x.strip() for x in self.coordinates.split(',')]
+
+        location     = geolocator.reverse(coords_list)
+        self.address = location.raw['address']   # dictionary
 
 
-    def set_city(self):
+    def set_city(self) -> None:
+        """Set the gym's city from address"""
+
+        if 'address' not in vars(self):
+            raise AttributeError('address was not set')
+
         city = None
 
         # most common options
@@ -106,18 +119,24 @@ class Gym:
                 city = self.address[option]
         
         # manually enter city name
-        if city is None:
+        # TODO: log the error
+        if not city:
             prompt = 'Enter CITY for `{}`:\t'.format(self.coordinates)
             city   = input(prompt).strip()
 
         self.city = city.lower()
 
 
-    def set_county(self):
+    def set_county(self) -> None:
+        """Set the gym's county from address"""
+
         try:
             county = self.address['county']
+        except AttributeError:
+            raise AttributeError('address was not set')
         except KeyError:
             # manually enter county name
+            # TODO: log the error
             prompt = 'Enter COUNTY for `{}`:\t'.format(self.coordinates)
             county = input(prompt).strip()
         
@@ -125,24 +144,32 @@ class Gym:
         self.county = county.removesuffix(' county')
 
 
-    def set_state(self):
-        self.state = self.address['state'].lower()
+    def set_state(self) -> None:
+        """Set the gym's state from address"""
+        try:
+           state = self.address['state']
+        except AttributeError:
+            raise AttributeError('address was not set')
+        except KeyError:
+            # manually enter state name (RARE)
+            # TODO: log the error
+            prompt = 'Enter STATE for `{}`:\t'.format(self.coordinates)
+            state = input(prompt).strip()
+
+        self.state = state.lower()
 
 
-    def format_fields(self) -> list:
-        '''
-        Construct custom list of attributes.
-        
+    def values(self) -> list:
+        """
         Returns
         -------
         fields:
-            The object's attribute values reorganized
-        '''
+            The object's attribute values w/o address
+        """
 
         fields = [
             v for k,v in vars(self).items() 
-            if k not in ['style', 'address']
+            if k not in ['address']
             ]
-        fields.insert(2, self.style)
 
         return fields
