77651f62f0ec9ba52d4212ee881f3252afc231b6
guerrad49 <63264459+guerrad49@users.noreply.github.com>
2024-04-27 19:29:21 -0400

    added examples and main docstring

--- PokemonGo/gym.py
+++ PokemonGo/gym.py
@@ -1,6 +1,18 @@
 """
 PokemonGo.gym
+-------------
+
+This module contains GoldGym class for compiling a gym's 
+data in one location such as:
+    1. Initial known gym values from database
+    2. Values read using PokemonGo.Image class
+    3. Methods which complete gym location values
+
+Once all gym values are set, the best use-case is by using
+`list(myGym)` as the data parameter of the `write_row`
+method of PokemonGo.GoogleSheet class.
 """
+
 
 from typing import Optional, Any
 
@@ -15,8 +27,24 @@ LONG_TERM_DEFENDING = 100   # in days
 
 class GoldGym:
     """
-    An instance of this class represents a container for 
-    managing gym-related information.
+    An instance of this class manages gym-related fields.
+
+    Examples
+    --------
+    >>> # instance w/ required parameter
+    >>> aa = GoldGym(1)
+    >>> 
+    >>> # instance w/ all optional parameters
+    >>> bb = GoldGym(uid=2,
+    ...     title='sydney opera house', victories=100,
+    ...     days=10, hours=7, minutes=20, treats=500)
+    >>> 
+    >>> # instance w/ unpacking dictionary of parameters
+    >>> params = {
+    ...     'title': '大阪城', 'victories': 246, 
+    ...     'days': 2, 'hours': 3, 'minutes': 40, 'treats': 369
+    ...     }
+    >>> cc = GoldGym(uid=3, **params)
     """
     
     def __init__(
@@ -29,15 +57,15 @@ class GoldGym:
         minutes:   Optional[int | str] = 0,
         treats:    Optional[int | str] = 0
     ) -> None:
-        self.uid       = self._int(uid)
+        self.uid       = self.__toint__(uid)
         self.title     = title
         self.style     = None
-        self.victories = self._int(victories)
-        self.days      = self._int(days)
-        self.hours     = self._int(hours)
-        self.minutes   = self._int(minutes)
+        self.victories = self.__toint__(victories)
+        self.days      = self.__toint__(days)
+        self.hours     = self.__toint__(hours)
+        self.minutes   = self.__toint__(minutes)
         self.defended  = 0
-        self.treats    = self._int(treats)
+        self.treats    = self.__toint__(treats)
         
         """
         Parameters
@@ -59,11 +87,20 @@ class GoldGym:
         """
 
 
-    # TODO: update name
-    def _int(self, x: Any) -> int:
-        """Convert argument to int when possible."""
+    def __toint__(self, x: Any) -> int:
+        """
+        Attempt argument conversion to integer for common 
+        data types.
+        """
 
-        # attempt str -> int
+        if isinstance(x, int):
+            return x
+
+        if isinstance(x, float):
+            if x == int(x):
+                return int(x)
+            raise ArgumentError
+        
         if isinstance(x, str):
             try:
                 int(x)
@@ -71,12 +108,49 @@ class GoldGym:
                 raise ArgumentError
             else:
                 return int(x)
-            
-        if not isinstance(x, int):
-            raise ArgumentError
 
-        return x
+        raise ArgumentError
     
+
+    def __iter__(self):
+        """
+        Overwritten to return iterable of object's 
+        attribute values EXCEPT `address` value.
+        """
+
+        for key,val in self.__dict__.items():
+            if key == 'address':
+                pass
+            else:
+                yield val
+    
+
+    def set_victories(self, x: int | str) -> None:
+        """Safe setting function for victories."""
+
+        self.victories = self.__toint__(x)
+    
+
+    def set_days(self, x: int | str) -> None:
+        """Safe setting function for days."""
+        
+        self.days = self.__toint__(x)
+    
+
+    def set_hours(self, x: int | str) -> None:
+        """Safe setting function for hours."""
+        self.hours = self.__toint__(x)
+    
+
+    def set_minutes(self, x: int | str) -> None:
+        """Safe setting function for minutes."""
+        self.minutes = self.__toint__(x)
+    
+
+    def set_treats(self, x: int | str) -> None:
+        """Safe setting function for treats."""
+        self.treats = self.__toint__(x)
+
 
     def set_defended(self) -> None:
         """
@@ -102,23 +176,23 @@ class GoldGym:
             self.style = 'gold'
 
 
-    def set_address(self, coordinates: str, email: str) -> None:
+    def set_address(self, latlon: str, email: str) -> None:
         """
         Set address dictionary.
 
         Parameters
         ----------
-        coordinates:
+        latlon:
             The known coordinates in `lat,long` format
         email:
             The user's email required by third party ToS
         """
 
-        self.coordinates  = coordinates
-        geolocator        = Nominatim(user_agent=email)
+        self.latlon  = latlon
+        geolocator   = Nominatim(user_agent=email)
 
         # proactive format clean up
-        coords_list  = [x.strip() for x in self.coordinates.split(',')]
+        coords_list  = [x.strip() for x in self.latlon.split(',')]
 
         location     = geolocator.reverse(coords_list)
         self.address = location.raw['address']   # dictionary
@@ -127,6 +201,7 @@ class GoldGym:
     def set_city(self) -> None:
         """
         Set the gym's city from address.
+        Note: May require user interface.
         
         See Also
         --------
@@ -146,7 +221,7 @@ class GoldGym:
         # manually enter city name
         # TODO: log the error
         if not city:
-            prompt = 'Enter CITY for `{}`:\t'.format(self.coordinates)
+            prompt = 'Enter CITY for `{}`:\t'.format(self.latlon)
             city   = input(prompt).strip()
 
         self.city = city.lower()
@@ -155,6 +230,7 @@ class GoldGym:
     def set_county(self) -> None:
         """
         Set the gym's county from address.
+        Note: May require user interface.
         
         See Also
         --------
@@ -168,7 +244,7 @@ class GoldGym:
         except KeyError:
             # manually enter county name
             # TODO: log the error
-            prompt = 'Enter COUNTY for `{}`:\t'.format(self.coordinates)
+            prompt = 'Enter COUNTY for `{}`:\t'.format(self.latlon)
             county = input(prompt).strip()
         
         county = county.lower()
@@ -178,6 +254,7 @@ class GoldGym:
     def set_state(self) -> None:
         """
         Set the gym's state from address.
+        Note: May require user interface.
         
         See Also
         --------
@@ -191,24 +268,7 @@ class GoldGym:
         except KeyError:
             # manually enter state name (RARE)
             # TODO: log the error
-            prompt = 'Enter STATE for `{}`:\t'.format(self.coordinates)
+            prompt = 'Enter STATE for `{}`:\t'.format(self.latlon)
             state = input(prompt).strip()
 
         self.state = state.lower()
-
-
-    # TODO: update name
-    def values(self) -> list:
-        """
-        Returns
-        -------
-        fields:
-            The object's attribute values w/o address
-        """
-
-        fields = [
-            v for k,v in vars(self).items() 
-            if k not in ['address']
-            ]
-
-        return fields
