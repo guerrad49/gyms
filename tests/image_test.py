cadd4159f3b4addab19d1ebe8b2305188371777e
guerrad49 <63264459+guerrad49@users.noreply.github.com>
2024-04-29 20:23:27 -0400

    added tests

--- tests/image_test.py
+++ tests/image_test.py
@@ -1,32 +1,50 @@
-import os
 import unittest
+import unittest.mock
 
-from PokemonGo.image import Image
+import pytest
+
+from PokemonGo.image import BadgeImage
 from PokemonGo.exceptions import UnsupportedPhoneModel
 
 
 class ImageTests(unittest.TestCase):
     def setUp(self):
-        self.base = os.path.join('tests', 'images')
+        self.im01 = BadgeImage('tests/images/IMG_0001.PNG')
+        self.im02 = BadgeImage('tests/images/IMG_1000.PNG')
+        self.im03 = BadgeImage('tests/images/IMG_1190.PNG')
 
-    def test_ise_image(self):
-        path = os.path.join(self.base, 'IMG_0001.PNG')
-        img  = Image(path)
-        self.assertEqual(img.scale, 1.75)
+    #==========================================================================
+    def test_empty_instance(self):
+        self.assertRaises(TypeError, BadgeImage)
 
-    def test_i11_image(self):
-        path = os.path.join(self.base, 'IMG_1000.PNG')
-        img  = Image(path)
-        self.assertEqual(img.scale, 1.5)
+    #==========================================================================
+    def test_image_ise(self):
+        self.assertEqual(self.im01.scale, 1.75)
 
-    def test_i15_image(self):
-        path = os.path.join(self.base, 'IMG_1190.PNG')
-        img  = Image(path)
-        self.assertEqual(img.scale, 1)
+    def test_image_i11(self):
+        self.assertEqual(self.im02.scale, 1.5)
 
-    def test_unsupported_image(self):
-        path = os.path.join(self.base, 'SHAKA.PNG')
-        self.assertRaises(UnsupportedPhoneModel, Image, path)
+    def test_image_i15(self):
+        self.assertEqual(self.im03.scale, 1)
+
+    def test_image_unsupported(self):
+        path = 'tests/images/SHAKA.PNG'
+        self.assertRaises(UnsupportedPhoneModel, BadgeImage, path)
+
+    #==========================================================================
+    def test_title(self):
+        self.assertEqual(self.im01.get_title(), 'third ward park playground')
+
+    #==========================================================================
+    def test_stats(self):
+        self.assertEqual(
+            self.im02.get_gym_activity(), 
+            {'victories':18, 'days':25, 'hours':18, 'minutes':1, 'treats':9}
+            )
+
+    def test_stats_manual(self):
+        unittest.mock.builtins.input = lambda _: ""
+        pass
 
 if __name__ == '__main__':
     unittest.main()
