6a22996dd4ead538bb990d6e4b4673e29bfab233
guerrad49 <63264459+guerrad49@users.noreply.github.com>
2024-05-13 20:08:12 -0400

    updated imports

--- PokemonGo/__init__.py
+++ PokemonGo/__init__.py
@@ -3,6 +3,6 @@
 __version__ = "0.1.0"
 __author__ = "David Guerra"
 
-from .google_sheet import GoogleSheet
-from .image import BadgeImage
+from .sheet import GymSheet
+from .image import GymBadge
 from .gym import GoldGym
