9a438663205780a2da3741c3e5386c76a1dd8ad9
guerrad49 <63264459+guerrad49@users.noreply.github.com>
2024-05-20 19:27:16 -0400

    added order to tests

--- tests/image_test.py
+++ tests/image_test.py
@@ -4,47 +4,66 @@ import unittest.mock
 import pytest
 
 from PokemonGo.image import GymBadge
-from PokemonGo.exceptions import UnsupportedPhoneModel
+from PokemonGo.exceptions import UnsupportedPhoneModel, InputError
 
 
 class ImageTests(unittest.TestCase):
     def setUp(self):
         self.im01 = GymBadge('tests/images/IMG_0001.PNG')
-        self.im02 = GymBadge('tests/images/IMG_1000.PNG')
-        self.im03 = GymBadge('tests/images/IMG_1190.PNG')
+        self.im02 = GymBadge('tests/images/IMG_0002.PNG')
+        self.im03 = GymBadge('tests/images/IMG_0003.PNG')
 
     #==========================================================================
+    @pytest.mark.order(1)
     def test_empty_instance(self):
         self.assertRaises(TypeError, GymBadge)
 
     #==========================================================================
-    def test_image_ise(self):
-        self.assertEqual(self.im01.scale, 1.75)
+    @pytest.mark.order(2)
+    def test_model_ise(self):
+        self.assertEqual(self.im01.model, 'iSE')
 
-    def test_image_i11(self):
-        self.assertEqual(self.im02.scale, 1.5)
+    @pytest.mark.order(3)
+    def test_model_i11(self):
+        self.assertEqual(self.im02.model, 'i11')
 
-    def test_image_i15(self):
-        self.assertEqual(self.im03.scale, 1)
+    @pytest.mark.order(4)
+    def test_model_i15(self):
+        self.assertEqual(self.im03.model, 'i15')
 
-    def test_image_unsupported(self):
+    @pytest.mark.order(5)
+    def test_model_unsupported(self):
         path = 'tests/images/SHAKA.PNG'
         self.assertRaises(UnsupportedPhoneModel, GymBadge, path)
 
     #==========================================================================
+    @pytest.mark.order(6)
     def test_title(self):
-        self.assertEqual(self.im01.get_title(), 'third ward park playground')
+        self.assertEqual(self.im03.get_title(), 'tulip mural')
 
     #==========================================================================
+    @pytest.mark.order(7)
     def test_activity(self):
         self.assertEqual(
-            self.im02.get_gym_activity(uid=1000), 
-            {'victories':18, 'days':25, 'hours':18, 'minutes':1, 'treats':9}
+            self.im03.get_gym_activity(), 
+            {'victories':21, 'days':17, 'hours':17, 'minutes':0, 'treats':501}
             )
 
-    def test_activity_manual(self):
-        unittest.mock.builtins.input = lambda _: ""
-        pass
+    @pytest.mark.order(8)
+    def test_activity_input_good(self):
+        # mock up user response
+        unittest.mock.builtins.input = lambda _: "11 19d 5h 1m 17"
+        self.im02.get_gym_activity()
+        self.assertEqual(self.im02.errors[0], 'STATS')
+    
+    @pytest.mark.order(9)
+    def test_activity_input_bad(self):
+        # mock up user response
+        unittest.mock.builtins.input = lambda _: "11 19d th 1m 17"
+        self.assertRaises(
+            InputError, self.im02.get_gym_activity
+            )
+
 
 if __name__ == '__main__':
     unittest.main()
