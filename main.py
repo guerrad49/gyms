f87a8a73bdafc9c23c4b93cf1c8f7945c09154ff
guerrad49 <63264459+guerrad49@users.noreply.github.com>
2024-05-20 15:15:38 -0400

    updated latest working version

--- main.py
+++ main.py
@@ -5,31 +5,33 @@ import os
 import sys
 import argparse
 
-from PokemonGo import utils
-from PokemonGo import GymSheet, GymBadge, GoldGym
+from PokemonGo import (
+    GymSheet, GymBadge, GoldGym, 
+    utils
+)
 
 
 #===============================FUNCTIONS=====================================
     
-def parse_args():
-    command_desc = 'Scan badge data from PNG files.'
-    p = argparse.ArgumentParser(description=command_desc)
+# def parse_args():
+#     command_desc = 'Scan badge data from PNG files.'
+#     p = argparse.ArgumentParser(description=command_desc)
 
-    flags = p.add_mutually_exclusive_group()
-    flags.add_argument('-s', '--scan', action='store_true',
-        help='scan new badges')
-    flags.add_argument('-u', '--update', action='store_true',
-        help='update badge given')
+#     flags = p.add_mutually_exclusive_group()
+#     flags.add_argument('-s', '--scan', action='store_true',
+#         help='scan new badges')
+#     flags.add_argument('-u', '--update', action='store_true',
+#         help='update badge given')
 
-    args = p.parse_args()
+#     args = p.parse_args()
             
-    return args
+#     return args
 
 
 #==================================MAIN=======================================
 
 if __name__ == '__main__':
-    args = parse_args()
+    # args = parse_args()
 
     utils.load_env()
     utils.set_logger()
@@ -47,25 +49,27 @@ if __name__ == '__main__':
 
     for id, path in zip(ids, queue):
         img = GymBadge(path)
-        imgData = img.get_title() | img.get_gym_activity(id)   # python3.9+
+        imgData  = {'title': img.get_title(), 'model': img.model}
+        imgData |= img.get_gym_activity()   # python3.9+
 
-        titleFromDf, ridx = gs.find(imgData['title'], id)
+        titleFromDf, ridx = gs.find(imgData['title'], False)
         imgData['title'] = titleFromDf
 
-        coords = gs.unprocessed.at[ridx,'latlon']            
+        coords = gs.processed.at[ridx,'latlon']
 
-        gg = GoldGym(id, **imgData)
-        gg.set_time_defended()
-        gg.set_style()
-        gg.set_address(coords, os.environ['EMAIL'])
-        gg.set_city()
-        gg.set_county()
-        gg.set_state()
+        gym = GoldGym(id, **imgData)
+        gym.set_time_defended()
+        gym.set_style()
+        gym.set_address(coords, os.environ['EMAIL'])
+        gym.set_city()
+        gym.set_county()
+        gym.set_state()
 
-        gymRow = list(gg)
         img.to_storage(os.environ['BADGES'], id)
-
-        gs.write_row(ridx, gymRow)
+ 
+        gs.write_row(ridx, gym)
+        errors = gs.errors + img.errors + gym.errors
+        utils.log_entry(id, errors)
         print()
-
+ 
     gs.geo_sort()
