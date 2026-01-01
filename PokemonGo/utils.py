46fe5a4d221f4e42d1438597407a1c98e6608a0e
guerrad49 <63264459+guerrad49@users.noreply.github.com>
2026-01-01 15:46:45 -0500

    updated documentation

--- PokemonGo/utils.py
+++ PokemonGo/utils.py
@@ -28,8 +28,13 @@ def parse_args():
 
 def are_similar(x: str, y: str) -> bool:
     """
-    Determine if string similarity is above a threshold.
-    WARNING: For short strings, user may want to decrease SIMILARITY_MIN.
+    Determine if two texts are at least 90% similar.
+
+    :param str x: The first text.
+    :param str y: The second text.
+
+    .. warning::
+        The similarity percentage may be too high for short strings.
     """
 
     likeness = SequenceMatcher(None, x, y).ratio()
@@ -45,21 +50,26 @@ def are_similar(x: str, y: str) -> bool:
 
 
 def load_env() -> None:
-    """Check and load package environment variables."""
+    """
+    Check and load package environment variables.
+    
+    :raises EnvironmentError: if environment file not loaded.
+    :raises FileNotFoundError: if json key file not found.
+    """
 
-    # get top level directory for package
+    # Get top level directory for package.
     packageDir = os.path.dirname(__file__)
     topDir     = os.path.dirname(packageDir)
 
     requirements = os.path.join(topDir, 'requirements')
 
-    # load environment
+    # Load environment.
     envPath = os.path.join(requirements, 'variables.env')
     config = dotenv_values(envPath)
     if '' in config.values() or None in config.values():
         raise EnvironmentError
     
-    # check json key file exits
+    # Check json key file exits.
     keyfile = os.path.join(requirements, config['JSON_KEY'])
     if not os.path.isfile(keyfile):
         raise FileNotFoundError
@@ -73,7 +83,12 @@ def load_env() -> None:
 
 
 def get_queue(verbose: bool) -> list:
-    """Returns queue of images to scan."""
+    """
+    Build a queue of images to scan.
+    
+    :param bool verbose: If True, print progress statements.
+    :returns: List of images to scan.
+    """
 
     downloads = os.environ['DOWNLOADS']
     queue = [
@@ -102,25 +117,18 @@ def set_logger() -> None:
         )
 
 
-def log_entry(gymUid: int, errors: list) -> None:
+def log_entry(gymId: int, errors: list) -> None:
     """
-    Compose message body and create entry in log.
+    Compose message body and create entry in log. The user should 
+    have previously called `meth:set_logger`.
 
-    Parameters
-    ----------
-    gymUid:
-        The unique id number identifying a gym
-    errors:
-        The list of errors i.e. CITY, TITLE, STATS, etc
-    
-    See Also
-    --------
-    utils.set_logger
+    :param int gymId: The unique id number identifying a gym.
+    :param list errors: The list of errors detected in submodules.
     """
 
     logger = logging.getLogger(__name__)
 
-    msg = 'ID: {:04}'.format(gymUid)
+    msg = 'ID: {:04}'.format(gymId)
     if errors:
         errStr = ', '.join(errors)
         msg += '   Errors: {}'.format(errStr)
